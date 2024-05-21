7c9fe2c0f6b499e207f2899ea87f34ea5acacf57
Namfon <varisa.vb@hotmail.com>
2024-05-21 13:57:59 +0700

    feat(ex-7)

--- ex-7.js
+++ ex-7.js
@@ -1,8 +1,10 @@
 function isPalindrome(string) {
   // Start coding here
+  let reversedString = string.split("").toReversed().join("");
+  return reversedString === string;
 }
 
 //Example case
-console.log(isPalindrome("reviver"));// true
-console.log(isPalindrome("บวบ"));// true
-console.log(isPalindrome("deliver"));// false
+console.log(isPalindrome("reviver")); // true
+console.log(isPalindrome("บวบ")); // true
+console.log(isPalindrome("deliver")); // false
