a0b0f2da1291fd2cc170a871efad95887d92200f
Namfon <varisa.vb@hotmail.com>
2024-05-21 13:51:05 +0700

    feat(ex-6)

--- ex-6.js
+++ ex-6.js
@@ -2,6 +2,15 @@ const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
   // Start coding here
+  let index = carCollection.indexOf(carBrand);
+  if (index === -1) {
+    carCollection.push(carBrand);
+    return `new car collection is : ${carCollection.join(",")}.`;
+  } else {
+    return `${carBrand} has already existed in the ${
+      index + 1
+    } position of car collection.`;
+  }
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
